3be1ffb9bf4a4f6246382084e48fe2175630a5bd
lulu <142243076+second0pition@users.noreply.github.com>
2024-10-30 11:03:41 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -5,7 +5,31 @@
 //chamada
 
 criaCartao(
-    'Programação'
-    'O que é JavaScript?'
+    'Programação',
+    'O que é JavaScript?',
     'O JavaScript é uma linguagem de Programação'
+)
+
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Biologia',
+    'O que é uma célula?',
+    'A célula é a unidade básica da vida, responsável por todas as funções vitais do organismo'
 )
